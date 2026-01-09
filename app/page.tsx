1394f71a6a40b208c06c2ff0ca476c5500dff237
Fredrik Etsare <fredrik@etsare.se>
2026-01-09 17:41:00 +0100

    fetch from 5 min to 10 min

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import DepartureBoard from "@/components/DepartureBoard";
 import { fetchDepartures } from "@/lib/actions";
 
-// Revalidate every 5 minutes - all users will share the same data
-export const revalidate = 300000;
+// Revalidate every 10 minutes - all users will share the same data
+// Client will refresh every 30 sec but with same data
+export const revalidate = 600000;
 
 export default async function Home() {
   const initialDepartures = await fetchDepartures();
