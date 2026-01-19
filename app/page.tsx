e1d8e380f41c78a0fe5afd4dade8a88b169f554b
Fredrik Etsare <fredrik@etsare.se>
2026-01-19 12:45:35 +0100

    cache revalidation for cache fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { fetchDepartures } from "@/lib/actions";
 
 // Revalidate every 10 minutes - all users will share the same data
 // Client will refresh every 30 sec but with same data
-export const revalidate = 600000;
 
 export default async function Home() {
   const initialDepartures = await fetchDepartures();
